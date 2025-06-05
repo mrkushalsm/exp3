9c98044b71fb874e883485ead6c38e7565d22fd5
Kushal SM <146661162+mrkushalsm@users.noreply.github.com>
2025-06-05 15:19:22 +0530

    Adding files

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("application")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass = "org.example.Main"
+}
+
+task copyWebsite(type: Copy) {
+    from "src/main/resource/website/*"
+    to "docs"
+}
